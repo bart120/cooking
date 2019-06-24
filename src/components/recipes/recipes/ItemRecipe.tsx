912bf7a67afe403551926ee696aeac0be71b32e7
Vincent <vleclerc@inow.fr>
2019-06-24 23:13:42 +0200

    liste des recette ok

---
+++ src/components/recipes/recipes/ItemRecipe.tsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { RecipeModel } from '../../../models/RecipeModel';
+import { GridListTile, GridListTileBar, IconButton } from '@material-ui/core';
+import InfoIcon from '@material-ui/icons/Info';
+import './ItemRecipe.css';
+
+interface ItemRecipeProps {
+    item: RecipeModel;
+}
+
+export default class ItemRecipe extends Component<ItemRecipeProps> {
+    render() {
+
+        return (
+            <GridListTile>
+                <img src={this.props.item.image} alt={this.props.item.name} className="img" />
+                <GridListTileBar
+                    title={this.props.item.name}
+                    subtitle={<span>by: moi</span>}
+                    actionIcon={
+                        <IconButton aria-label={`info about ${this.props.item.name}`} className="icon">
+                            <InfoIcon />
+                        </IconButton>
+                    }
+                />
+            </GridListTile>
+        );
+    }
+}
---
+++ src/models/RecipeModel.tsx
@@ -0,0 +1,5 @@
+export interface RecipeModel {
+    id: number;
+    name: string;
+    image?: string;
+} 
