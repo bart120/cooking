b8c9366cd71a3034b24543537f3af5650c74b1de
Vincent <vleclerc@inow.fr>
2019-06-24 21:55:03 +0200

    recipes

--- src/components/recipes/recipes/Recipes.tsx
+++ src/components/recipes/recipes/Recipes.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Card, CardHeader, Avatar, IconButton, CardMedia, CardContent, Typography, CardActions, Collapse, makeStyles, Theme, createStyles } from '@material-ui/core';
-import classes from '*.module.css';
 import clsx from 'clsx';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
@@ -34,6 +33,18 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 export default class Recipes extends Component {
+
+    state = {
+        items: [
+            { id: 1, name: 'recette1', image: '' },
+            { id: 2, name: 'recette2', image: '' },
+            { id: 3, name: 'recette3', image: '' },
+            { id: 4, name: 'recette4', image: '' }
+        ]
+    };
+
+    
+
     recipeReviewCard = () => {
         const classes = useStyles();
         const [expanded, setExpanded] = React.useState(false);
@@ -42,79 +53,86 @@ export default class Recipes extends Component {
             setExpanded(!expanded);
         }
 
+        
+
+
         return (
-            <Card className={classes.card}>
-                <CardHeader
-                    avatar={
-                        <Avatar aria-label="Recipe" className={classes.avatar}>
-                            R
+            <div>
+                {this.state.items.map((item, index) => (
+                    <Card className={classes.card}>
+                        <CardHeader
+                            avatar={
+                                <Avatar aria-label="Recipe" className={classes.avatar}>
+                                    R
                 </Avatar>
-                    }
-                    action={
-                        <IconButton aria-label="Settings">
-                            <MoreVertIcon />
-                        </IconButton>
-                    }
-                    title="Shrimp and Chorizo Paella"
-                    subheader="September 14, 2016"
-                />
-                <CardMedia
-                    className={classes.media}
-                    image="/static/images/cards/paella.jpg"
-                    title="Paella dish"
-                />
-                <CardContent>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        This impressive paella is a perfect party dish and a fun meal to cook together with your
-                        guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                            }
+                            action={
+                                <IconButton aria-label="Settings">
+                                    <MoreVertIcon />
+                                </IconButton>
+                            }
+                            title={item.name}
+                            subheader="September 14, 2016"
+                        />
+                        <CardMedia
+                            className={classes.media}
+                            image="/images/recipe.png"
+                            title="Paella dish"
+                        />
+                        <CardContent>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                This impressive paella is a perfect party dish and a fun meal to cook together with your
+                                guests. Add 1 cup of frozen peas along with the mussels, if you like.
               </Typography>
-                </CardContent>
-                <CardActions disableSpacing>
-                    <IconButton aria-label="Add to favorites">
-                        <FavoriteIcon />
-                    </IconButton>
-                    <IconButton aria-label="Share">
-                        <ShareIcon />
-                    </IconButton>
-                    <IconButton
-                        className={clsx(classes.expand, {
-                            [classes.expandOpen]: expanded,
-                        })}
-                        onClick={handleExpandClick}
-                        aria-expanded={expanded}
-                        aria-label="Show more"
-                    >
-                        <ExpandMoreIcon />
-                    </IconButton>
-                </CardActions>
-                <Collapse in={expanded} timeout="auto" unmountOnExit>
-                    <CardContent>
-                        <Typography paragraph>Method:</Typography>
-                        <Typography paragraph>
-                            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                            minutes.
+                        </CardContent>
+                        <CardActions disableSpacing>
+                            <IconButton aria-label="Add to favorites">
+                                <FavoriteIcon />
+                            </IconButton>
+                            <IconButton aria-label="Share">
+                                <ShareIcon />
+                            </IconButton>
+                            <IconButton
+                                className={clsx(classes.expand, {
+                                    [classes.expandOpen]: expanded,
+                                })}
+                                onClick={handleExpandClick}
+                                aria-expanded={expanded}
+                                aria-label="Show more"
+                            >
+                                <ExpandMoreIcon />
+                            </IconButton>
+                        </CardActions>
+                        <Collapse in={expanded} timeout="auto" unmountOnExit>
+                            <CardContent>
+                                <Typography paragraph>Method:</Typography>
+                                <Typography paragraph>
+                                    Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                                    minutes.
                 </Typography>
-                        <Typography paragraph>
-                            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-                            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-                            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                                <Typography paragraph>
+                                    Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+                                    heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+                                    browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+                                    and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+                                    pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+                                    saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
                 </Typography>
-                        <Typography paragraph>
-                            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-                            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-                            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-                            minutes more. (Discard any mussels that don’t open.)
+                                <Typography paragraph>
+                                    Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+                                    without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+                                    medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+                                    again without stirring, until mussels have opened and rice is just tender, 5 to 7
+                                    minutes more. (Discard any mussels that don’t open.)
                 </Typography>
-                        <Typography>
-                            Set aside off of the heat to let rest for 10 minutes, and then serve.
+                                <Typography>
+                                    Set aside off of the heat to let rest for 10 minutes, and then serve.
                 </Typography>
-                    </CardContent>
-                </Collapse>
-            </Card>
+                            </CardContent>
+                        </Collapse>
+                    </Card>
+                ))}
+            </div>
         );
 
     }
@@ -122,6 +140,9 @@ export default class Recipes extends Component {
 
 
     render() {
-        return (<this.recipeReviewCard />);
+        return (
+
+            <this.recipeReviewCard />
+        );
     }
 }
