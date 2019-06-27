be44375c67789975c308112c19c52ee6052267fa
Vincent <vleclerc@inow.fr>
2019-06-27 13:03:23 +0200

    prepare docker

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ItemRecipe from './components/recipes/recipes/ItemRecipe';
+import { RecipeModel } from './models/RecipeModel';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('tests sur APP', () => {
+
+  //beforeAll
+  //beforeEach
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+
+
 });
